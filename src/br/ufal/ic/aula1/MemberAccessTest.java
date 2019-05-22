4c415dc3bdedd301084d59a6dad0aa253f9ce232
Rodrigo Lima <rodrigosan.lima@gmail.com>
2019-05-22 11:45:47 -0300

    member access and this test

---
+++ src/br/ufal/ic/aula1/MemberAccessTest.java
@@ -0,0 +1,13 @@
+package br.ufal.ic.aula1;
+
+public class MemberAccessTest {
+
+    public static void main(String[] args) {
+        Time1 time = new Time1();
+
+        time.hour = 1; // error: hour private
+        time.minute = 15; // error: minute private
+        time.second = 30; // error: second private
+    }
+
+}
